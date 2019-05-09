cd173243a9de0babd18b7dda0c882f0e220a93ff
lukeclee <33207332+lukeclee@users.noreply.github.com>
2019-05-08 23:38:16 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HomePage from './HomePage';
+import NavBar from './NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
